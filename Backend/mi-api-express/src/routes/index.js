6c922038af9138d5ed93acc1ba79320e30f54402
DevLaRoca02 <developer02lrgt@outlook.com>
2025-04-21 20:00:53 -0600

    patch

--- Backend/mi-api-express/src/routes/index.js
+++ Backend/mi-api-express/src/routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const router = express.Router();
 
 const setRoutes = (app) => {
+
+    const router = express.Router();
+
     router.get('/', (req, res) => {
         res.send('Bienvenido a la API de Express');
     });
 
     // Aquí puedes agregar más rutas según sea necesario
 
-    app.use('/api', router);
+    app.use('/', router);
 };
 
-module.exports = setRoutes;
+module.exports = { setRoutes };
